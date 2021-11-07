f26402c3e5be01fd8d915528bda2df885fdd5609
Skylar Harwell <swharwell@gmail.com>
2021-11-07 17:44:17 -0500

    File structure commit

---
+++ myportfolio/src/components/main.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import Landing from './landing';
+
+// const Main = () => {
+//     <Switch>
+//         <Route exact path='/' component={Landing} />
+//     </Switch>
+// }
+
+// export default Main;
